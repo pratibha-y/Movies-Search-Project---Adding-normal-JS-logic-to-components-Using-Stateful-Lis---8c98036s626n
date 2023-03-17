058ffa31262048cb315d9a4ff6d6c4a645887641
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-03-17 17:24:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,11 @@ import "../styles/App.css";
 import { movies } from "../utils/movieList";
 
 const App = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [results, setResult] = useState(null);
+    const [inputValue, setInputValue] = useState("");
+    const [results, setResult] = useState(null);
 
-  function handleClick(e) {
-    e.preventDefault();
+    function handleClick(e) {
+        e.preventDefault();
     const searchTerm = inputValue.toLowerCase();
     const filteredMovies = movies.filter(
       (movie) => movie.toLowerCase().includes(searchTerm)
@@ -15,29 +15,52 @@ const App = () => {
     setResult(filteredMovies);
   }
 
-  function handleChange(e) {
-    setInputValue(e.target.value);
-  }
+    
 
-  return (
-    <div id="main">
-      <form id="form">
-        <input
-          type="text"
-          onChange={handleChange}
-          value={inputValue}
-          id="movie-input"
-        ></input>
-        <button id="button" onClick={handleClick}>
-          Search
-        </button>
-      </form>
-      <div id="result">
-        {results &&
-          results.map((movie) => <div className="movie">{movie}</div>)}
-      </div>
-    </div>
-  );
+    function handleChange(event) { 
+
+        const latestInputeValue = event.target.value;
+        setInputValue(latestInputeValue);
+
+        if(latestInputeValue ==""){
+            setResult(null);
+            return;
+        };
+
+        const serachRegex = new RegExp(latestInputeValue,"i");
+
+
+        const searchFilteredMovies = movies.filter((movie)  => {
+            const doesMatchSearch = serachRegex.test(movie.title);
+            return doesMatchSearch;
+
+        });
+        setResult(searchFilteredMovies);
+
+
+    }
+
+    return (
+        <div id="main">
+            <form id="form">
+                <input
+                    type="text"
+                    onChange={handleChange}
+                    value={inputValue}
+                    id="movie-input"
+                ></input>
+                <button id="button" onClick={handleClick}>
+                    Search
+                </button>
+            </form>
+            <div id="result">
+                {results &&
+                    results.map((movie) => (
+                        <div className="movie" key = {movie.id}>{movie.title}</div>
+                    ))}
+            </div>
+        </div>
+    );
 };
 
 export default App;
